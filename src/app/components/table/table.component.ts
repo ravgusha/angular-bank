0accc7c4a527f0b1c4cea45e27893b65dd29ab67
ravgusha <ravgusha@gmail.com>
2022-10-12 23:37:50 +0300

    refactor: add loop for nav items

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import data from '../../data/db.json';
 
 interface IUser {
